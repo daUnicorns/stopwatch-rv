73ff54aa05e88cae3dce466e7127c6ac54a6d413
RobStallion <Robertefrancis18@gmail.com>
2016-01-28 12:31:34 +0000

    Added 2 new tests and some js code to pass those tests. Also added some styling to the index.html

--- test/test.js
+++ test/test.js
@@ -1,11 +1,45 @@
-test("timeElapsed should be Zero before we start the Timer", function() {
-  equal(zero(), 0, 'Message: timer is zero at start' );
+test('Calling `getTime` on a new `Stopwatch` should return `0`.', function( assert ) {
+  var watch = new Stopwatch();
+  var expectedTime = 0;
+  var actualTime = watch.getTime();
+
+  assert.equal(actualTime, expectedTime);
 });
 
-test('timer should count from 0 to 1', function() {
-  equal(counter(), 1, 'Incremented zero by one');
-});
+test('Calling `getTime` after the timer has been running for 2000ms should return `2000`.', function( assert ) {
+  var done = assert.async();
+  var expectedTime = 2000;
 
-test('timer should count from 0 to 2', function() {
-  equal(increment(), 2, 'Incremented zero by two');
+  var watch = new Stopwatch();
+  watch.start();
+
+  setTimeout(function() {
+
+    watch.stop();
+    var actualTime = Math.round(watch.getTime()/100)*100;
+    assert.equal(actualTime, expectedTime);
+    done();
+
+  }, expectedTime);
 });
+// test("timeElapsed should be Zero before we start the Timer", function() {
+//   equal(zero(), 0, 'Message: timer is zero at start' );
+// });
+//
+// test('timer should count from 0 to 1', function() {
+//   equal(counter(), 1, 'Incremented zero by one');
+// });
+//
+
+// test('minute goes from 0 to 1', function() {
+//   equal(time(), 2, 'Incremented minute by one');
+// });
+//
+// test('minute goes from 0 to 1', function(assert) {
+//   assert.expect(1);
+//   var done = assert.async();
+//   time(function() {
+//     assert.ok(true, "test made it to one minute");
+//     done();
+//   }, 200);
+// });
